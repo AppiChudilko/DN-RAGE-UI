6cc062c22e70ab339131c7496bd1dd3ece30d877
Alexander <channelappi@gmail.com>
2020-01-27 17:13:25 +0300

    - Фикс бага с даблкликом

--- src/components/phone/components/apps/UMenuComponetns/UFrame.jsx
+++ src/components/phone/components/apps/UMenuComponetns/UFrame.jsx
@@ -65,7 +65,7 @@ class UFrame extends React.Component {
           }
           {(this.props.type === 1) ?
             <React.Fragment>
-              <div className="umenu-mini-box" onClick={() => this.buttonClick(this.props.item.params)}>
+              <div className="umenu-mini-box">
               <div className="l-title">
                 {this.props.item.img === '' || this.props.item.img === undefined ? null :
                   <React.Fragment>
