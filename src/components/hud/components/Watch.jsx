76a87a43a60b7c171752f6aa71e0757cb477c74d
Alexander <channelappi@gmail.com>
2020-02-12 23:17:47 +0300

    - Добавил иконки зеленой и желтой зоны

--- src/components/hud/components/Watch.jsx
+++ src/components/hud/components/Watch.jsx
@@ -6,6 +6,8 @@ class Watch extends React.Component {
     super(props)
     this.state = {
       show: true,
+      showGreen: true,
+      showYellow: true,
       time: '14:00',
       date: '13.01.2020',
       temp: '+20*C',
@@ -23,6 +25,8 @@ class Watch extends React.Component {
         this.setState({time: value.time});
         this.setState({date: value.date});
         this.setState({temp: value.temp});
+        this.setState({showGreen: value.showGreen});
+        this.setState({showYellow: value.showYellow});
         this.setState({background: value.background});
       }
       else return;
@@ -35,6 +39,10 @@ class Watch extends React.Component {
     }
     return (
       <React.Fragment >
+        <div className="zone-box">
+          <div className={ this.state.showGreen ? 'time-img-greenzone' : 'hide'}></div>
+          <div className={ this.state.showYellow ? 'time-img-yellowzone' : 'hide'}></div>
+        </div>
         <div className="watch-main" style={{backgroundColor: 'rgba(0, 0, 0, ' + this.state.background + ')'}}>
           <div className="time-box">
             <div className="time">{this.state.time} <div className="time-img-watch"></div></div>
