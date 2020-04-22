77ec32cf96294563541560efcf74291db637f96c
Alexander <channelappi@gmail.com>
2020-04-22 19:26:13 +0300

    - Меню по цветам

--- src/components/hud/components/MainMenu/const/index.js
+++ src/components/hud/components/MainMenu/const/index.js
@@ -1,10 +1,12 @@
 export const COLORS = {
     w: '#ffffff',
+    s: '#ffffff',
     bl: '#000000',
     r: '#ff2b2b',
     b: '#1f75fe',
     g: '#0bda51',
-    y: '#ffcc00'
+    y: '#ffcc00',
+    o: '#ffcc00'
 }
 
 export const SPECIAL = {
