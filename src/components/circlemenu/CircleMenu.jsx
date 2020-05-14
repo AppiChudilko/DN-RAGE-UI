0c507758bb328250b511e861fa130315d6eb1dc6
Daniil <daniil-letovalcev@yandex.ru>
2020-05-14 14:06:11 +0300

    fix

--- src/components/circlemenu/CircleMenu.jsx
+++ src/components/circlemenu/CircleMenu.jsx
@@ -11,8 +11,8 @@ class CircleMenu extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            show: true,
-            showMenu: true,
+            show: false,
+            showMenu: false,
             selected: 0,
             selectedType: 0,
             menuData: [
