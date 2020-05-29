9fb55b90eb4a1e6ffcf6de5645f8a7470a624e6b
Daniil <48149254+LetikGit@users.noreply.github.com>
2020-05-29 21:48:05 +0300

    фикс бага со смещением

--- src/components/hud/components/MainMenu.jsx
+++ src/components/hud/components/MainMenu.jsx
@@ -242,7 +242,7 @@ class MainMenu extends React.Component {
 
     componentDidMount() {
         try {
-            this.itemRefs[0].focus()
+            this.itemRefs[0].scrollIntoView({ behavior: 'smooth', block: 'nearest' })
         } catch (e) {}
 
         /*let that = this; //DEBUG
@@ -271,7 +271,7 @@ class MainMenu extends React.Component {
                 this.setState({menuList: value.menuList});
                 this.setState({menuName: value.menuName});
                 try {
-                    this.itemRefs[value.selected].focus();
+                    this.itemRefs[value.selected].scrollIntoView({ behavior: 'smooth', block: 'nearest' })
                 }
                 catch (e) {}
             } else return;
