f8dd46a02d64c24173812392ecb1c0cfaae85693
Alexander <channelappi@gmail.com>
2020-08-02 19:38:37 +0300

    - Фиксы

--- src/components/hudmenus/tatoo/components/CatalogItem.jsx
+++ src/components/hudmenus/tatoo/components/CatalogItem.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import '../css/item.css'
 
-const CatalogItem = ({ name, setSelected, sale }) => (
+const CatalogItem = ({ name, setSelected, sale, desc }) => (
     <div className="tatoo__content__list__item" onClick={setSelected}>
         <span className="tatoo__content__list__item__name">
             {name}
+            {desc && (
+                <span className="tatoo__content__list__item__desc">
+                <br />{`${desc}`}
+                </span>
+            )}
         </span>
         {sale > 0 && (
-        <span className="tatoo__content__list__item__sale">
-            {`- ${sale}%`}
-        </span>
+            <span className="tatoo__content__list__item__sale">
+                {`${sale}%`}
+            </span>
         )}
     </div>
 )
